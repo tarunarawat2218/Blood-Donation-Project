e9e082c80a632c7d450f92a73983b87a1fb733b7
Taruna Rawat <tarunarawat2218@gmail.com>
2022-04-23 23:40:30 +0530

    commit

--- routes/donar.js
+++ routes/donar.js
@@ -8,5 +8,8 @@ router.use(express.static(path.join(__dirname, '/public')));
 router.use('/', function(req, res, next){
     res.render('donar', {pageTitle: "Donar Registration"})
 });
+router.post('/submit',(req,res,next)=>{
+    res.redirect('/submit')
+})
 
 module.exports = router;
