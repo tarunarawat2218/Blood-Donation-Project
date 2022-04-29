6c50d3c6c1fcbaeef3f818afccc516d1f3cc4dfa
Taruna Rawat <tarunarawat2218@gmail.com>
2022-04-29 23:26:12 +0530

    commit

--- util/database.js
+++ util/database.js
@@ -4,7 +4,8 @@ const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
     database: 'nodejs',
-    password: 'tarunarawat2218'
+    password: '',
+    
 })
 
 module.exports = pool.promise();
